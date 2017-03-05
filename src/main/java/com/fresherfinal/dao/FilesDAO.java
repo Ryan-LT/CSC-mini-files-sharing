94e7a87a6f17647084c425c9be90ed8b1b8c6577
vuminhvuongit <vuminhvuongit@gmail.com>
2017-03-05 14:59:26 +0700

    [ ADD FILE] I have added some file DAO, DAOImpl in package com.fresherfinal.dao and com.fresherfinal.DAOImpl and a dependency in pom.xml. I don't change anything before and my file + code in it not interac source init.

---
+++ src/main/java/com/fresherfinal/dao/FilesDAO.java
@@ -0,0 +1,7 @@
+package com.fresherfinal.dao;
+
+import com.fresherfinal.dto.Files;
+
+public interface FilesDAO extends InterFaceDAO<Files> {
+	
+}
---
+++ src/main/java/com/fresherfinal/dao/HistoryUploadDAO.java
@@ -0,0 +1,7 @@
+package com.fresherfinal.dao;
+
+import com.fresherfinal.dto.HistoryUpload;
+
+public interface HistoryUploadDAO extends InterFaceDAO<HistoryUpload> {
+
+}
