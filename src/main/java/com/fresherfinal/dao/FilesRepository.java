f896342cb05e86ada72530b8e1a68df3bcc83d9b
vuminhvuongit <vuminhvuongit@gmail.com>
2017-03-07 09:54:16 -0500

    [edited properties]

--- src/main/java/com/fresherfinal/dao/FilesRepository.java
+++ src/main/java/com/fresherfinal/dao/FilesRepository.java
@@ -1,9 +1,13 @@
 package com.fresherfinal.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.fresherfinal.dto.Files;
 
 public interface FilesRepository extends JpaRepository<Files, Integer> {
-	
+//	@Query(value="SELECT ", nativeQuery=true)
+//	List<Files> findFiles(String infoFile);
 }
